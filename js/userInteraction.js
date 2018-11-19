7c65e8e7956ec6c4553023bef1358daf20147371
Linhong <linhong.zhou@mihsnyc.org>
2018-11-19 18:29:23 -0500

    11/19/18

--- js/userInteraction.js
+++ js/userInteraction.js
@@ -1,2 +1,21 @@
 // Where the questions are stored.
-export {};
+// export {};
+var database = firebase.database();
+var ref = database.ref("/");
+
+console.log("lol");
+$("#questionBtn").click(function(){
+    console.log("FIRE");
+    ref.push({
+        question:{text: $("#questionInput").val(), user: $("#questionUsername").val()}
+    });
+    display();
+})
+
+function display(){
+    $("body").html(
+        // snapshot.forEach((a) => {
+        //     console.log(a.val().question.text);
+        // });
+    );
+}
